66525c0f51ea756acfe1d86a7b10c187c7d0e5c8
root <suraj.phawade1@gmail.com>
2025-09-12 21:04:21 +0200

    sub & intr id change

--- dev/provider.tf
+++ dev/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   # backend "azurerm" {
   #   resource_group_name = "3tire-rg"                                  
-  #   tenant_id            = "768c2942-6ded-4274-a906-05c932e5a3d8"  
+  #   tenant_id            = "fbf3517a-6193-4d03-9580-ce8a94c1e28c"  
   #   storage_account_name = "3tirestg"                              
   #   container_name       = "tfstate"                               
   #   key                  = "dev.terraform.tfstate"                
@@ -15,6 +15,6 @@ terraform {
 }
 provider "azurerm" {
     features { }
-    subscription_id = "2e333137-3307-4678-a744-a2d37508c632"
+    subscription_id = "a7275015-536e-4c99-8431-5c9bfbc026c9"
   
 }
