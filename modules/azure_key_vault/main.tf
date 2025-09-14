f7607a9d60d2adacdc256a651065419ec2da1114
root <suraj.phawade1@gmail.com>
2025-09-14 21:05:49 +0200

    code is improve

--- modules/azure_key_vault/main.tf
+++ modules/azure_key_vault/main.tf
@@ -9,8 +9,16 @@ access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
 
+    key_permissions = [
+      "Get",
+    ]
+
     secret_permissions = [
-    "Get", "List", "Set", "Delete", "Recover", "Backup", "Restore"
-  ]
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
   }
 }
